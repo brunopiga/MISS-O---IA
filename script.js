4b0d6c6c734be0b897846359461344094569536a
brunopiga <137805772+brunopiga@users.noreply.github.com>
2025-05-27 08:35:52 -0300

    rehb

--- script.js
+++ script.js
@@ -5,24 +5,36 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-  {
-    enunciado: "qual time é maior que o palmeiras?",
-    alternativas: ["Flamengo", "São Paulo"],
-  },
-  {
-    enunciado: "Qual o melhor FIFA que existiu?",
-    alternativas: ["FIFA 17", "FIFA 14"],
-  },
-  {
-    enunciado: "Quem é mais ladrão?",
-    alternativas: ["Lula", "var do palmeiras"],
-  },
-  {
-    enunciado: "Quem chuta melhor?",
-    alternativas: ["Neymar Jr", "Raphinha"],
-  },
-  {
-    enunciado: "Qual jogador mais feio?",
-    alternativas: ["Vagner Love", "Frank Ribery"],
-  },
+{
+enunciado: "qual é o maior time do mundo ",
+alternativas: ["real madrid ", " Barcelona"],
+},
+{
+enunciado: "qual é o jogor mais bonito",
+alternativas: ["Cristiano Ronaldo", "neymar Jr"],
+},
+{
+enunciado: "qual é o jogador mais feio",
+alternativas: ["Ronaldinho", "Ronaldo fenômeno"],
+},
+{
+enunciado: "quem é mais corno",
+alternativas: ["de bruyne", "coringa"],
+},
+{
+enunciado: "qual é melhor finalizador da historia",
+alternativas: ["Neymito", " Caça Rato "],
+},
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta() {
+perguntaAtual = perguntas[atual];
+caixaPerguntas.textContent = perguntaAtual.enunciado;
+mostraAlternativas();
+}
+function mostraAlternativas() {}
+for (const alternativa of perguntaAtual.alternativas) {
+}
+
+mostraPergunta();
 ];
