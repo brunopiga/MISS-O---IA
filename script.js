b0306b2407368decf18ab8c0b332885959871d7c
brunopiga <137805772+brunopiga@users.noreply.github.com>
2025-06-03 11:20:52 -0300

    uivby

--- script.js
+++ script.js
@@ -7,24 +7,33 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
 {
 enunciado: "qual é o maior time do mundo ",
-alternativas: ["real madrid ", " Barcelona"],
+alternativas: ["real madrid ", " Barcelona"],  
+ texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
 },
 {
 enunciado: "qual é o jogor mais bonito",
-alternativas: ["Cristiano Ronaldo", "neymar Jr"],   texto: "Isso é assustador!",
+alternativas: ["Cristiano Ronaldo", "neymar Jr"],  
+ texto: "Isso é assustador!",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
 },
 {
 enunciado: "qual é o jogador mais feio",
-alternativas: ["Ronaldinho", "Ronaldo fenômeno"],
+alternativas: ["Ronaldinho", "Ronaldo fenômeno"],  
+ texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
 },
 {
 enunciado: "quem é mais corno",
-alternativas: ["de bruyne", "coringa"],
+alternativas: ["de bruyne", "coringa"],  
+ texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
 },
 {
 enunciado: "qual é melhor finalizador da historia",
-alternativas: ["Yuri aberto", " Caça Rato "],
+alternativas: ["Yuri aberto", " Caça Rato "],  
+ texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
 },
 let atual = 0;
 let perguntaAtual;
